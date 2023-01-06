f852f1f6729e8a50f7f1b313b384bf6d59ecb055
jarrel <jarrel.folly-adjon@edu.devinci.fr>
2023-01-06 12:54:47 +0100

    recherche add

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+let album = document.querySelector('.modal-form')
+let open = document.querySelector('.modal-open')
+let close = document.querySelector('.modal-close')
 
+open.addEventListener('click', function(){
+  album.classList.remove('hidden')
+})
+
+close.addEventListener('click', function(){
+  album.classList.add('hidden')
+})
 
 
 $(document).ready(() => {
